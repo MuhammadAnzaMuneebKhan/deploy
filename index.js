77f09da15c9f1f3514110e6b6b3a00912244e10e
MuhammadAnzaMuneebKhan <99421649+MuhammadAnzaMuneebKhan@users.noreply.github.com>
2023-11-18 22:11:44 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/** @format */
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/test', (req, res) => {
+  res.send('Hello test!');
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
